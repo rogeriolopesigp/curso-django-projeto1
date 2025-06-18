018c29da65fc538ac6bf9afd5206d1d7e24ef43a
rogeriolopesigp <rogeriolopesigp@gmail.com>
2025-06-18 22:58:35 +0100

    Atualizacao menu

--- recipes/models.py
+++ recipes/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.text import slugify
+import os
+from django.conf import settings
+from PIL import Image
 
 
 # Create your models here.
@@ -38,9 +41,30 @@ class Recipe(models.Model):
     def get_absolute_url(self):
         return reverse("recipes-recipe", args=(self.id,))
     
+    @staticmethod
+    def resize_image(image, new_width=800):
+        image_full_path = os.path.join(settings.MEDIA_ROOT, image.name)
+        image_pillow = Image.open(image_full_path)
+        original_width, original_height = image_pillow.size
+
+        if original_width <= new_width:
+            image_pillow.close()
+            return
+        new_height = round((new_width * original_height) / original_width)
+        new_image = image_pillow.resize((new_width, new_height), Image.LANCZOS)
+        new_image.save(
+            image_full_path,
+            optimize=True,
+            quality=50,
+        )
+    
     def save(self, *args, **kwargs):
         if not self.slug:
-            slug = f'{slugify(self.title)}'
-            self.slug = slug
-            
-        return super().save(*args, **kwargs)
+            self.slug = slugify(self.title)
+
+        # Salva o objeto primeiro (isso grava o arquivo)
+        super().save(*args, **kwargs)
+
+        # Depois redimensiona a imagem (agora ela existe no disco)
+        if self.cover:
+            self.resize_image(self.cover, 800)
